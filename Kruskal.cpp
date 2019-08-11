ec7bb0b675aebc99d90b6856c57cff5c93381412
Plamena Todorova <plamena.i.todorova@gmail.com>
2019-08-11 23:06:49 +0300

    Add files via upload

---
+++ Kruskal.cpp
@@ -0,0 +1,82 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<set>
+
+using namespace std;
+
+struct Node
+{
+	int sorce;
+	int destination;
+	int path;
+
+	bool operator<(const Node& n) const
+	{
+		return path < n.path;
+	}
+};
+
+std::vector<Node> Kruskal(std::vector<Node>& edges, int numberOfVertexes)
+{
+	std::vector<Node> tree;
+	std::vector<set<int>> unions(numberOfVertexes);
+
+	std::sort(edges.begin(), edges.end());
+
+	for (int i = 0; i < edges.size(); i++)
+	{
+		cout << edges[i].sorce << " " << edges[i].destination << " " << edges[i].path << endl;
+	}
+
+	int taken = 0, index = 0;
+	
+	while (taken < numberOfVertexes - 1)
+	{
+		Node currentEdge = edges[index++];
+
+		std::vector<int> result;
+		std::vector<int>::iterator it;
+
+		it = std::set_intersection(unions[currentEdge.sorce].begin(), unions[currentEdge.sorce].end(),
+			unions[currentEdge.destination].begin(), unions[currentEdge.destination].end(),
+			result.begin());
+
+		if (it - result.begin() == 0)
+		{
+			tree.push_back(currentEdge);
+			unions[currentEdge.sorce].insert(currentEdge.destination);
+			unions[currentEdge.destination].insert(currentEdge.sorce);
+		}
+	}
+
+	return tree;
+}
+
+int main()
+{
+	vector<Node> edges(11);
+	
+	for (int i = 0; i < 11; i++)
+	{
+		cin >> edges[i].sorce >> edges[i].destination >> edges[i].path;
+	}
+
+	cout << endl;
+
+	for (int i = 0; i < edges.size(); i++)
+	{
+		cout << edges[i].sorce << " " << edges[i].destination << " " << edges[i].path << endl;
+	}
+
+	cout << endl;
+
+	vector<Node> result = Kruskal(edges, 7);
+
+	for (int i = 0; i < result.size(); i++)
+	{
+		cout << result[i].sorce << " " << result[i].destination << " " << result[i].path << endl;
+	}
+
+	system("pause");
+}
